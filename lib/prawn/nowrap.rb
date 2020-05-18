a401f747d8edf6753b05a28435c7ea2281383bd0
Katsuya Hidaka <hidakatsuya@gmail.com>
2020-05-19 01:51:21 +0900

    initialize

---
+++ lib/prawn/nowrap.rb
@@ -0,0 +1,8 @@
+require "prawn/nowrap/version"
+
+module Prawn
+  module Nowrap
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/prawn/nowrap/version.rb
@@ -0,0 +1,5 @@
+module Prawn
+  module Nowrap
+    VERSION = "0.1.0"
+  end
+end
