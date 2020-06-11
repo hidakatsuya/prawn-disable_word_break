842d5924d5fb8eca4adf8366b5943523d0f2fd28
Katsuya HIDAKA <hidakatsuya@gmail.com>
2020-06-11 21:08:05 +0900

    trim final new lines

--- lib/prawn/disable_word_break.rb
+++ lib/prawn/disable_word_break.rb
@@ -7,7 +7,6 @@ require_relative 'disable_word_break/formatted_box'
 Prawn::DisableWordBreak::Disabler.tap { |mod|
   Prawn::Document.include mod unless Prawn::Document.include?(mod)
 }
-
 Prawn::DisableWordBreak::FormattedBox.tap { |mod|
   Prawn::Text::Formatted::Box.prepend mod unless Prawn::Text::Formatted::Box.include?(mod)
 }
