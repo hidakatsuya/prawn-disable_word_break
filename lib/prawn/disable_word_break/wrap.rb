79617cf9ee894b71e974aa2fc1b8dd35957bb23b
Katsuya Hidaka <hidakatsuya@gmail.com>
2023-11-13 00:39:32 +0900

    Add disable_word_break option to Text::Box and Formatted::Box

--- lib/prawn/disable_word_break/wrap.rb
+++ lib/prawn/disable_word_break/wrap.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
-require_relative 'line_wrap'
+require_relative 'line_break_anywhere'
 
 module Prawn
   module DisableWordBreak
     module Wrap
       # override
-      def initialize(*)
+      def initialize(_, options)
         super
 
-        @line_wrap = DisableWordBreak::LineWrap.new
+        @line_wrap = LineBreakAnywhere.new if DisableWordBreak.config.default || options[:disable_word_break]
+      end
+
+      # override
+      def valid_options
+        super + %i(disable_word_break)
       end
     end
   end
